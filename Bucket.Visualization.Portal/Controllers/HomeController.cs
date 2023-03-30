b4be1faf99b212fcd2e37e2a43fd79c76cf34d5f
Yang Tian <v-yangtian@micosoft.com>
2023-03-30 22:53:45 +0800

    update

--- Bucket.Visualization.Portal/Controllers/HomeController.cs
+++ Bucket.Visualization.Portal/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Bucket.Visualization.Portal.Controllers
             return View();
         }
 
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
